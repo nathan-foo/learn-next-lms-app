da7f14d484f0fc97c2c747f6bb556632c2146f16
Nathan Foo <ntfoo27@outlook.com>
2024-09-10 20:49:43 -0500

    Course Creation UI, Prisma and DB Setup

--- lms-app/app/(dashboard)/_components/logo.tsx
+++ lms-app/app/(dashboard)/_components/logo.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export const Logo = () => {
     return (
         <Image
-            height={32}
-            width={32}
+            height={160}
+            width={160}
             alt="logo"
             src="/logo.svg"
         />
